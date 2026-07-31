e1c556e5929e6b00dbc70f6b13140d2059549760
Sayan Das <dassayan018@gmail.com>
2026-07-31 12:53:23 +0000

    models/staging/orders/stg_orders__orders.sql _update sql

--- models/staging/orders/stg_orders__orders.sql
+++ models/staging/orders/stg_orders__orders.sql
@@ -1,24 +1,28 @@
-with source as (
-    select *
-    from {{ source('raw_orders', 'orders') }}
-),
-
-cleaned as (
-    select
-        order_id,
-        customer_id,
-        upper(order_status) as order_status,
-        upper(order_channel) as order_channel,
-        upper(payment_method) as payment_method,
-        initcap(delivery_city) as delivery_city,
-        initcap(delivery_state) as delivery_state,
-        round(discount_amount, 2) as discount_amount,
-        round(delivery_fee, 2) as delivery_fee,
-        order_placed_at,
-        created_at,
-        updated_at
-from source
-)
-
-select *
-from cleaned;
+with 
+  source as (
+    select 
+      * 
+    from 
+      {{ source('raw_orders', 'orders') }}
+  ), 
+  cleaned as (
+    select 
+      order_id, 
+      customer_id, 
+      upper(order_status) as order_status, 
+      upper(order_channel) as order_channel, 
+      upper(payment_method) as payment_method, 
+      initcap(delivery_city) as delivery_city, 
+      initcap(delivery_state) as delivery_state, 
+      round(discount_amount, 2) as discount_amount, 
+      round(delivery_fee, 2) as delivery_fee, 
+      order_placed_at, 
+      created_at, 
+      updated_at 
+    from 
+      source
+  ) 
+select 
+  * 
+from 
+  cleaned
